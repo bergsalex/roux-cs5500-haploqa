6878708c0cb0887534ee0b9216cdb139b100255b
Alexander Berger <Alexander.Berger@jax.org>
2021-11-14 14:09:04 -0400

    Adding two component stubs

---
+++ libs/ngx-genome-karyotype-plots/src/lib/genome-interval-plot/genome-interval-plot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-genome-interval-plot',
+  templateUrl: './genome-interval-plot.component.html',
+  styleUrls: ['./genome-interval-plot.component.scss']
+})
+export class GenomeIntervalPlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ libs/ngx-genome-karyotype-plots/src/lib/genome-karyotype-plot/genome-karyotype-plot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-genome-karyotype-plot',
+  templateUrl: './genome-karyotype-plot.component.html',
+  styleUrls: ['./genome-karyotype-plot.component.scss']
+})
+export class GenomeKaryotypePlotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
