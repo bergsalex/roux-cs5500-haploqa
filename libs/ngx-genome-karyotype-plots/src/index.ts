51a548fc1a4d791d5ea3a728d608c4d16ea0d242
Alexander Berger <Alexander.Berger@jax.org>
2021-12-06 10:04:51 -0500

    forgot to update index.ts w mm10 move

--- libs/ngx-genome-karyotype-plots/src/index.ts
+++ libs/ngx-genome-karyotype-plots/src/index.ts
@@ -1,4 +1,4 @@
 export * from './lib/ngx-genome-karyotype-plots.module';
-export { mm10ChrSizes } from "./lib/mm10-data";
+export { mm10ChrSizes } from "./lib/example-data/mm10-data";
 export * from "./lib/example-data/sample_ADG";
 export * from "./lib/example-data/sample_34M";
